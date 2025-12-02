7811ca56d51060f7101cdd4d05a89b51fbd2668c
Ohal Ayush Sandip <64695904+ayushohal@users.noreply.github.com>
2025-12-02 10:18:59 +0530

    Update assgn13.py

--- assgn13.py
+++ assgn13.py
@@ -6,9 +6,6 @@ df = pd.read_csv("Covid Vaccine Statewise.csv")
 # Clean column names (remove extra spaces)
 df.columns = df.columns.str.strip()
 
-pd.set_option('display.width', 200)   # increase width
-pd.set_option('display.max_columns', None)  # show all columns
-pd.set_option('display.float_format', '{:.2f}'.format)
 
 print("Columns:", df.columns.tolist())
 print("=== Dataset Description ===")
